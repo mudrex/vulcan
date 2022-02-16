fc092b457e2fc4e7ef9b4baef845f996267de8cf
grandwizard28 <vibhupandey28@gmail.com>
2022-02-16 20:17:21 +0530

    fix(wait): Run prettier and add comments

--- src/cmd/wait.js
+++ src/cmd/wait.js
@@ -14,45 +14,43 @@ exports.command = 'wait [options]'
 /**
  * Description of the command
  */
-exports.describe = 'Wait till the deployment is in STEADY_STATE.'
+exports.describe =
+	'Waits until a deployment becomes healthy. It polls ECS API at a specified interval until a specified time.'
 
 /**
  * Builder of the command
  */
 exports.builder = {
 	'service-name': {
-		describe: 'Name of ECS service in the specified AWS region.',
+		describe:
+			'The short name or full Amazon Resource Name (ARN) of the service in which the task set is in.',
 		type: 'string',
 		demandOption: true,
 	},
 	'cluster-name': {
-		describe: 'Name of ECS cluster in the specified AWS region.',
+		describe:
+			'The short name or full Amazon Resource Name (ARN) of the cluster in which the task set is in.',
 		type: 'string',
 		demandOption: true,
 	},
 	'green-task-set-arn': {
-		describe: 'ARN of the task set.',
+		describe: 'The Amazon Resource Name (ARN) of the green task set.',
 		type: 'string',
 		default: 'task.json',
 		demandOption: true,
 	},
 	'timeout': {
-		describe: 'Timeout (in seconds) for the new task to become healthy',
+		describe:
+			'The total time (in seconds) to wait for the deployment to become healthy. The command will fail if the deployment fails to become healthy within this time period.',
 		type: 'number',
 		default: '100',
 		demandOption: false,
 	},
 	'poll-interval': {
 		describe:
-			'Interval (in seconds) in which ECS will be polled to get the task status.',
+			'The interval (in seconds) in which ECS API is polled. The status of the deployment will be retrieved at this interval.',
 		type: 'number',
 		default: '10',
-		demandOption: false,
-	},
-	'check': {
-		describe: 'Check whether deployment is still in steady state.',
-		type: 'boolean',
-		default: false,
 		demandOption: false,
 	},
 }
@@ -61,7 +59,7 @@ exports.builder = {
  * Handler which executes on this command.
  *
  * The AWS ECS API is polled every poll-interval seconds for timeout seconds.
- * If the green-task-set-arn is in STEADY_STATE, command is successful.
+ * If the deployment becomes healthy in that timeout, the command is successfull.
  *
  * @param {Object} argv - The parameters mentioned in {@link module:wait.builder}
  *
@@ -74,41 +72,29 @@ exports.handler = async (argv) => {
 			argv.clusterName,
 			argv.greenTaskSetArn
 		)
+		if (taskStatus.taskSets.length == 0) {
+			const errorMessage = `Error occurred while describing task set with arn ${taskStatus.failures[0].arn}, reason is ${taskStatus.failures[0].reason}`
+			logger.error(`${errorMessage}`)
+			throw Error(`${errorMessage}`)
+		}
 		const taskSet = taskStatus.taskSets[0]
 		logger.debug(`Green Task Set: ${JSON.stringify(taskSet, null, 2)}`)
 		const taskSetStabilityStatus = taskSet.stabilityStatus
 		logger.info(
-			`Task Set ${argv.greenTaskSetArn} has stabilityStatus ${taskSetStabilityStatus}`
+			`Task set ${argv.greenTaskSetArn} has stability status ${taskSetStabilityStatus}`
 		)
-		if (argv.check == false && taskSetStabilityStatus == 'STEADY_STATE') {
+		if (taskSetStabilityStatus == 'STEADY_STATE') {
 			logger.debug('Clearing ecsTaskStatusId')
-			logger.info(`Deployment has reached status ${taskSetStabilityStatus}`)
+			logger.info(`Task set has reached status ${taskSetStabilityStatus}`)
 			clearInterval(ecsTaskStatusId)
 			clearTimeout(globalTimeoutId)
-		}
-
-		if (argv.check == true && taskSetStabilityStatus != 'STEADY_STATE') {
-			logger.debug('Clearing ecsTaskStatusId')
-			clearInterval(ecsTaskStatusId)
-			clearTimeout(globalTimeoutId)
-			const errorMessage = `Deployment has become unhealthy with status ${taskSetStabilityStatus}.`
-			logger.error(`${errorMessage}`)
-			throw new Error(`${errorMessage}`)
 		}
 	}, argv.pollInterval * 1000)
 
 	const globalTimeoutId = setTimeout(() => {
 		clearInterval(ecsTaskStatusId)
-		if (argv.check == false) {
-			const errorMessage = `Deployment failed to reach steady state within ${argv.timeout} seconds.`
-			logger.error(`${errorMessage}`)
-			throw new Error(`${errorMessage}`)
-		}
-
-		if (argv.check == true) {
-			logger.info(
-				`Deployment has been steady for the past ${argv.timeout} seconds.`
-			)
-		}
+		const errorMessage = `Task set ${argv.greenTaskSetArn} failed to reach steady state within ${argv.timeout} seconds`
+		logger.error(`${errorMessage}`)
+		throw new Error(`${errorMessage}`)
 	}, argv.timeout * 1000)
 }
